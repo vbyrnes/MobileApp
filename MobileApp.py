51070db81a09c203f0d2605af8d50458c37ce243
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-27 06:49:15 -0500

    Update MobileApp.py

--- MobileApp.py
+++ MobileApp.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text='tech with me')
+        return Label(text="tech with me")
 
 if __name__=="__main__":
     MyApp().run()
