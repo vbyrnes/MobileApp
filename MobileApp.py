58d9da60dd44084200164f30bf46868f9a4a203a
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-27 07:29:31 -0500

    Update MobileApp.py

--- MobileApp.py
+++ MobileApp.py
@@ -1,11 +1,33 @@
 import kivy
 from kivy.app import App 
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols=2
+        self.add_widget(Label(text="First Name: "))
+        self.firstname=TextInput(multiline=False)
+        self.add_widget(self.firstname)
+
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols=2
+        self.add_widget(Label(text="Last Name: "))
+        self.lastname=TextInput(multiline=False)
+        self.add_widget(self.lastname)
+
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols=2
+        self.add_widget(Label(text="Email: "))
+        self.email=TextInput(multiline=False)
+        self.add_widget(self.email)
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="tech with me")
+        return MyGrid()
 
 if __name__=="__main__":
     MyApp().run()
