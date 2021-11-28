64be02c0d93ef049dcbdce963e078d71f273c41a
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-27 23:51:47 -0500

    Update MobileApp.py

--- MobileApp.py
+++ MobileApp.py
@@ -8,24 +8,26 @@ from kivy.uix.button import Button
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols=2
+
+        self.inside = GridLayout()
+        self.cols = 2
         self.add_widget(Label(text="First Name: "))
-        self.firstname=TextInput(multiline=False)
+        self.firstname = TextInput(multiline=False)
         self.add_widget(self.firstname)
 
         super(MyGrid, self).__init__(**kwargs)
-        self.cols=2
+        self.cols = 2
         self.add_widget(Label(text="Last Name: "))
-        self.lastname=TextInput(multiline=False)
+        self.lastname = TextInput(multiline=False)
         self.add_widget(self.lastname)
 
         super(MyGrid, self).__init__(**kwargs)
-        self.cols=2
+        self.cols = 2
         self.add_widget(Label(text="Email: "))
-        self.email=TextInput(multiline=False)
+        self.email = TextInput(multiline=False)
         self.add_widget(self.email)
 
-        self.submit= Button(text="submit", font_size=40)
+        self.submit = Button(text="submit", font_size=40)
         self.add_widget(self.submit)
 
 class MyApp(App):
